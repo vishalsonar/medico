cc881cc392c603a7ff2742dcafd42122773810bf
vishalsonar <vsonar908@gmail.com>
2021-10-10 19:30:04 +0530

    login test case update

--- medico.core/src/test/java/com/sonar/vishal/medico/core/TestLogin.java
+++ medico.core/src/test/java/com/sonar/vishal/medico/core/TestLogin.java
@@ -5,11 +5,13 @@ import java.security.NoSuchAlgorithmException;
 
 import org.junit.Test;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.sonar.vishal.medico.common.message.common.Constant;
 import com.sonar.vishal.medico.common.message.common.Message;
 import com.sonar.vishal.medico.common.pojo.Role;
-import com.sonar.vishal.medico.common.pojo.User;
+import com.sonar.vishal.medico.common.structure.RoleData;
 import com.sonar.vishal.medico.common.structure.UserData;
 
 public class TestLogin extends UnitTest {
@@ -20,20 +22,26 @@ public class TestLogin extends UnitTest {
 
 	@Test
 	public void test() throws InvalidKeyException, NoSuchAlgorithmException {
-		TestApi(data.getAddRoleRequest());
-		JsonObject response = TestApi(data.getAllRoleRequest());
-		JsonObject roleObject = response.get(Constant.DATA).getAsJsonObject().get(Constant.LIST).getAsJsonArray().get(0).getAsJsonObject();
-		Role role = gson.fromJson(roleObject, Role.class);
-		assertNotNull(role);
-		Message message = data.getAddUserRequest(role);
+		Role role = null;
+		Message message = data.getAddRoleRequest();
+		RoleData roleData = (RoleData) message.getData();
+		roleData.getRole().setName("role1");
 		TestApi(message);
+		JsonObject response = TestApi(data.getAllRoleRequest());
+		JsonArray responseArray = response.get(Constant.DATA).getAsJsonObject().get(Constant.LIST).getAsJsonArray();
+		for (JsonElement element : responseArray) {
+			role = gson.fromJson(element.getAsJsonObject(), Role.class);
+			if (role.getName().equals("role1")) {
+				break;
+			}
+		}
+		assertNotNull(role);
+		message = data.getAddUserRequest(role);
 		UserData userData = (UserData) message.getData();
-		response = TestApi(data.getAllUserRequest());
-		JsonObject userObject = response.get(Constant.DATA).getAsJsonObject().get(Constant.LIST).getAsJsonArray().get(0).getAsJsonObject();
-		User user = gson.fromJson(userObject, User.class);
-		assertNotNull(user);
-		user.setPassword(userData.getUser().getPassword());
-		TestApi(data.getLoginData(user));
+		userData.getUser().setUserName("admin1");
+		userData.getUser().getRole().setId(role.getId());
+		TestApi(message);
+		TestApi(data.getLoginData(userData.getUser()));
 	}
 
 }
